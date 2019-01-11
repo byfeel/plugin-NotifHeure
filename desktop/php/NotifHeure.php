f1d2f4b663d33a752bf7f5122e08c237802804f5
byfeel <infobyfeel@gmail.com>
2019-01-11 09:04:01 +0100

    bug widget

--- desktop/php/NotifHeure.php
+++ desktop/php/NotifHeure.php
@@ -60,7 +60,7 @@ $eqLogics = eqLogic::byType($plugin->getId());
                      style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
                     <i class="fa fa-plus-circle" style="font-size : 6em;color:#0970b9;"></i>
                     <br>
-                    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#94ca02">{{Ajouter}}</span>
+                    <span style="font-size : 1.1em;position:relative; top : 23px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;color:#0970b9">{{Ajouter}}</span>
                 </div>
                 <div class="cursor eqLogicAction" data-action="gotoPluginConf"
                      style="text-align: center; background-color : #ffffff; height : 120px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;">
